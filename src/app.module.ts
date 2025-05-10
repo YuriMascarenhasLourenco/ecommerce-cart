dd35c118e0cefd4d3601246230d9e0c56dedc937
Yuri Lourenço <lourenco99yuri@gmail.com>
2025-05-10 02:17:15 -0300

    fix: PORT

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { PaymentModule } from './payment/payment.module';
     TypeOrmModule.forRoot({
       type: process.env.DB_USERNAME ? 'postgres' : 'postgres',
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT ? 5432 : 5432,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DATABASE,
